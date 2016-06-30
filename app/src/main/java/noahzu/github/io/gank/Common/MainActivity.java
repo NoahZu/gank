bed13ec4b94093720c36da4f64be2cb4d07b61b6
NoahZu <1010138814@qq.com>
2016-06-30 15:51:56 +0800

    修复了点击toolbar上的home菜单不能打开drawer的bug

--- app/src/main/java/noahzu/github/io/gank/Common/MainActivity.java
+++ app/src/main/java/noahzu/github/io/gank/Common/MainActivity.java
@@ -3,6 +3,7 @@ package noahzu.github.io.gank.Common;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -58,6 +59,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 
     private void initCommitGank() {
         mDrawerLayout.closeDrawers();
