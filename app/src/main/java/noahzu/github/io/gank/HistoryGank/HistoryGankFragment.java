8f1573de9ef6ca1787c902de9e7c9d65bc2d3932
俎金好 <zujinhao@yixia.com>
2016-08-15 18:53:07 +0800

    使用库简化历史gank

--- app/src/main/java/noahzu/github/io/gank/HistoryGank/HistoryGankFragment.java
+++ app/src/main/java/noahzu/github/io/gank/HistoryGank/HistoryGankFragment.java
@@ -1,22 +1,17 @@
 package noahzu.github.io.gank.HistoryGank;
 
 
-import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ProgressBar;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import noahzu.github.io.gank.Base.BaseFragment;
-import noahzu.github.io.gank.Data.entity.Gank;
 import noahzu.github.io.gank.Data.entity.HistoryGankResult;
 import noahzu.github.io.gank.R;
 import noahzu.github.io.gank.widget.RecycleViewDivider;
@@ -49,7 +44,7 @@ public class HistoryGankFragment extends BaseFragment implements HistoryGankCont
 
     @Override
     protected void initData() {
-        adapter = new HistoryGankListAdapter(getContext(),new ArrayList<HistoryGankResult.PreviewGank>(0));
+        adapter = new HistoryGankListAdapter(getContext(),R.layout.his_gank_item,new ArrayList<HistoryGankResult.PreviewGank>(0));
         gankListRecylerView.setAdapter(adapter);
     }
 
@@ -85,7 +80,7 @@ public class HistoryGankFragment extends BaseFragment implements HistoryGankCont
 
     @Override
     public void showGanks(List<HistoryGankResult.PreviewGank> ganks) {
-        adapter.refreshData(ganks);
+        adapter.setNewData(ganks);
     }
 
     @Override
