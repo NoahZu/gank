b91f8ee8705bbecbd430482118d59ec621b87804
俎金好 <zujinhao@yixia.com>
2017-06-02 09:35:08 +0800

    no message

---
+++ app/src/main/java/noahzu/github/io/gank/Data/entity/BeanWrapper.kt
@@ -0,0 +1,9 @@
+package noahzu.github.io.gank.Data.entity
+
+/**
+ * Created by Administrator on 2016/8/15.
+ */
+class BeanWrapper<T> {
+    var isError: Boolean = false
+    var beans: List<T> = null
+}
---
+++ app/src/main/java/noahzu/github/io/gank/Data/entity/KotlinBeanWrapper.kt
@@ -0,0 +1,9 @@
+package noahzu.github.io.gank.Data.entity
+
+/**
+ * Created by zujinhao on 17/5/22.
+ */
+class KotlinBeanWrapper<T>{
+    var error: Boolean
+
+}
